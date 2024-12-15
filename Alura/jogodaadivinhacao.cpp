0f00d97d58979853b8c5681f93432829590a634c
brunoramossilva <brunogabriel0900@gmail.com>
2024-12-15 20:13:08 -0300

    Att

--- Alura/jogodaadivinhacao.cpp
+++ Alura/jogodaadivinhacao.cpp
@@ -12,5 +12,5 @@ int main () {
     cout << "*************************************" << endl;
 
     int numero_secreto = 42;
-    
+    cout << "O número secreto é " << numero_secreto << ". Não conte para ninguém!" << endl;
 }
