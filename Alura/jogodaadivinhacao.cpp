383065879c1590c1c39963d93bc320b25957a8ef
brunoramossilva <brunogabriel0900@gmail.com>
2025-01-01 19:25:40 -0300

    Att.

--- Alura/jogodaadivinhacao.cpp
+++ Alura/jogodaadivinhacao.cpp
@@ -8,9 +8,14 @@ int main () {
     setlocale(LC_ALL, "Portuguese");
 
     cout << "*************************************" << endl;
-    cout << "* Bem-vindo ao jogo da adivinhação! *" << endl;
+    cout << "* Bem-vindo(a) ao jogo da adivinhação! *" << endl;
     cout << "*************************************" << endl;
 
     int numero_secreto = 42;
     cout << "O número secreto é " << numero_secreto << ". Não conte para ninguém!" << endl;
+
+    int chute;
+    cin >>  chute;
+    cout <<"O valor do seu chute é "<< chute << endl;
+
 }
