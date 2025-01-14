a74b0430e298fbe1c8f1b9769bccad99289273a0
brunoramossilva <brunogabriel0900@gmail.com>
2025-01-14 20:50:29 -0300

    Att

--- Alura/Jogo-da-Forca/jogodaforca.cpp
+++ Alura/Jogo-da-Forca/jogodaforca.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 #include <string>
 #include <map>
+#include <cctype>
 #include <locale.h>
+
 using namespace std;
 
 const string PALAVRA_SECRETA = "MELANCIA";
+
 map<char, bool> chutou;
 
 bool letra_existe(char chute){
-    // for(int i = 0; i < PALAVRA_SECRETA.size(); i++ ){
-        //if(chute == PALAVRA_SECRETA[i]){
-            //return true;
-        //}
+
+    /* Outra forma de executar a interação e verificação: */
+    /*for(int i = 0; i < PALAVRA_SECRETA.size(); i++ ){
+        if(chute == PALAVRA_SECRETA[i]){
+            return true;*/
 
         for (char letra : PALAVRA_SECRETA){
             if (chute == letra){
@@ -21,15 +25,12 @@ bool letra_existe(char chute){
         }
         return false;
     }
-//}
 
 
 int main(){
 
     setlocale(LC_ALL, "Portuguese");
 
-
-    cout << PALAVRA_SECRETA << endl;
 
     bool nao_acertou = true;
     bool nao_enforcou = true;
@@ -41,15 +42,14 @@ int main(){
             }
             else {
                 cout << "_ ";
-
             }
-
         }
         cout << endl;
 
 
         char chute;
         cin >> chute;
+        chute = toupper(chute);
 
         chutou[chute] = true;
 
