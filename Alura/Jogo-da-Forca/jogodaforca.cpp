fba903a6aeca6e2430e1f99d0f9a8905de696327
brunoramossilva <brunogabriel0900@gmail.com>
2025-01-18 14:44:10 -0300

    Modificações na estrutura do código.

--- Alura/Jogo-da-Forca/jogodaforca.cpp
+++ Alura/Jogo-da-Forca/jogodaforca.cpp
@@ -22,10 +22,12 @@ vector <char> chutes_errados;
 // Função que diz se a letra chutada existe ou não na palavra secreta:
 bool letra_existe(char chute){
 
-    /* Outra forma de executar a interação e verificação: */
-    /*for(int i = 0; i < palavra_secreta.size(); i++ ){
+    /* Outra forma de executar a interação e verificação:
+    for(int i = 0; i < palavra_secreta.size(); i++ ){
         if(chute == palavra_secreta[i]){
-            return true;}*/
+            return true;
+        }
+    }*/
 
         for (char letra : palavra_secreta){
             if (chute == letra){
