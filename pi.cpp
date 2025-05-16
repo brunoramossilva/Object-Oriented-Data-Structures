884fbd89ee44578b129dd87e8dd5dc1e9bfe654e
brunoramossilva <brunogabriel0900@gmail.com>
2025-05-16 12:58:22 -0300

    Att.

---
+++ pi.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>  // Para usar string
+using namespace std;
+
+int main() {
+    int a = 5, b = 10, c = 2;
+    float pi = 3.14;
+    string nome = "João";
+
+    cout << "Soma de a e b: " << a + b << endl;
+    cout << "Valor de pi: " << pi << endl;
+    cout << "Nome: " << nome << endl;
+
+    int resultado = a * b + c;
+    cout << "Resultado: " << resultado << endl;
+
+    return 0;
+}
